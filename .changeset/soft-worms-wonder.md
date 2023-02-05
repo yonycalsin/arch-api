df9f8c66205e02a0a9dedd601b99927458cfec43
Yony Calsin <helloyonycalsin@gmail.com>
2023-02-05 18:02:41 -0500

    feat: add action, middleware and responder definitions to the rest package

---
+++ .changeset/soft-worms-wonder.md
@@ -0,0 +1,5 @@
+---
+'@arch-api/rest': minor
+---
+
+add action, middleware and responder definitions to the rest package
--- packages/rest/src/index.ts
+++ packages/rest/src/index.ts
@@ -1,1 +1,3 @@
-export {}
+export type { default as Responder } from './Definitions/Responder/Responder'
+export type { default as Action } from './Definitions/Action/Action'
+export type { default as Middleware } from './Definitions/Middleware/Middleware'
